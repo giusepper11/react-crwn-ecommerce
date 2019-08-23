06147e58f021d11c57cc481177a5c6a0b294a2da
Giuseppe Rosa <giusepper11@gmail.com>
2019-08-23 14:06:00 -0300

    Finalizando componentes de login

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import './sign-in.style.scss'
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    };
+
+    handelChange = event => {
+        const {value, name} = event.target;
+        this.setState({[name]: value})
+    };
+
+    render() {
+        return (
+            <div className='sign-in'>
+
+                <h2 className='title'>Já tenho conta</h2>
+                <span>Entre com seu login e senha</span>
+
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email" name='email' label='email' value={this.state.email}
+                               handleChange={this.handelChange} required/>
+
+                    <FormInput type="password" name='password' label='senha' value={this.state.password}
+                               handleChange={this.handelChange}
+                               required/>
+
+
+                    <CustomButton type="submit">LOGAR</CustomButton>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
